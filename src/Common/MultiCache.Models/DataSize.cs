55028f974888bc43b3d877c418e989342623a5d5
XorZy <>
2021-12-14 23:00:28 +0100

    introduced DataSize

---
+++ src/Common/MultiCache.Models/DataSize.cs
@@ -0,0 +1,110 @@
+namespace MultiCache.Models
+{
+    using MultiCache.Utils;
+    using System;
+    using System.Text.RegularExpressions;
+
+    public readonly record struct DataSize : IComparable<DataSize>
+    {
+        private readonly static Regex _splitRegex = new Regex(@"(?<=\d)(?=[\sa-zA-Z])", RegexOptions.Compiled);
+        public long Bits { get; }
+        public static DataSize Parse(string input)
+        {
+            input = input.Trim();
+            var split = _splitRegex.Split(input);
+            if (split.Length != 2)
+            {
+                throw new ArgumentException("Invalid format");
+            }
+
+            var quantity = split[0].ToDoubleInvariant();
+            var unit = split[1].Trim();
+
+            int power = unit[0] switch
+            {
+                'b' or 'B' => 0,
+                'k' or 'K' => 1,
+                'm' or 'M' => 2,
+                'g' or 'G' => 3,
+                't' or 'T' => 4,
+                _ => throw new FormatException("Unknown format")
+            };
+
+            int bitIndex = power == 0 ? 0 : 1;
+            int baseUnit = 1000;
+
+            if (unit[1] == 'i') // IEC prefix
+            {
+                baseUnit = 1024;
+                bitIndex = 2;
+            }
+
+            int bitMultiplier = unit[bitIndex] switch
+            {
+                'b' => 1,
+                'B' => 8,
+                _ => throw new FormatException("Unknown format")
+            };
+
+            long bps = (long)Math.Round(
+
+                Math.Pow(baseUnit, power) * bitMultiplier * quantity);
+
+            return new DataSize(bps);
+        }
+
+        public DataSize(long bits)
+        {
+            Bits = bits;
+        }
+
+        public override string ToString()
+        {
+            if (Bits < 1024)
+            {
+                return $"{Bits}b";
+            }
+            else if (Bits < 1024 * 1024)
+            {
+                return $"{Bits / 1024.0}Kib";
+            }
+            else if (Bits < 1024 * 1024 * 1024)
+            {
+                return $"{Bits / (1024.0 * 1024.0)}Mib";
+            }
+            else if (Bits < 1024L * 1024 * 1024 * 1024)
+            {
+                return $"{Bits / (1024.0 * 1024.0 * 1024.0)}Gib";
+            }
+            else
+            {
+                return $"{Bits / (1024.0 * 1024.0 * 1024.0 * 1024.0)}Tib";
+            }
+        }
+
+        public int CompareTo(DataSize other)
+        {
+            return Bits.CompareTo(other.Bits);
+        }
+
+        public static bool operator <(DataSize left, DataSize right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(DataSize left, DataSize right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(DataSize left, DataSize right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(DataSize left, DataSize right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
--- src/Common/MultiCache.Network/Speed.cs
+++ src/Common/MultiCache.Network/Speed.cs
@@ -1,88 +1,28 @@
 namespace MultiCache.Network
 {
-    using MultiCache.Utils;
     using System;
-    using System.Text.RegularExpressions;
+    using MultiCache.Models;
 
     public readonly record struct Speed : IComparable<Speed>
     {
-        private readonly static Regex _splitRegex = new Regex(@"(?<=\d)(?=[\sa-zA-Z])", RegexOptions.Compiled);
-        public long BitsPerSecond { get; }
-
-        public bool IsUnlimited => BitsPerSecond < 1;
-        public static Speed Parse(string input)
-        {
-            input = input.Trim();
-            var split = _splitRegex.Split(input);
-            if (split.Length != 2)
-            {
-                throw new ArgumentException("Invalid format");
-            }
-
-            var quantity = split[0].ToDoubleInvariant();
-            var unit = split[1].Trim();
-
-            int power = unit[0] switch
-            {
-                'b' or 'B' => 0,
-                'k' or 'K' => 1,
-                'm' or 'M' => 2,
-                'g' or 'G' => 3,
-                't' or 'T' => 4,
-                _ => throw new FormatException("Unknown format")
-            };
-
-            int bitIndex = power == 0 ? 0 : 1;
-            int baseUnit = 1000;
-
-            if (unit[1] == 'i') // IEC prefix
-            {
-                baseUnit = 1024;
-                bitIndex = 2;
-            }
-
-            int bitMultiplier = unit[bitIndex] switch
-            {
-                'b' => 1,
-                'B' => 8,
-                _ => throw new FormatException("Unknown format")
-            };
-
-            long bps = (long)Math.Round(
-
-                Math.Pow(baseUnit, power) * bitMultiplier * quantity);
-
-            return new Speed(bps);
-        }
 
         public Speed(long bps)
         {
-            BitsPerSecond = bps;
+            _size = new DataSize(bps);
         }
 
-        public override string ToString()
+        public Speed(DataSize bps)
         {
-            if (BitsPerSecond < 1024)
-            {
-                return $"{BitsPerSecond}bps";
-            }
-            else if (BitsPerSecond < 1024 * 1024)
-            {
-                return $"{BitsPerSecond / 1024.0}Kibps";
-            }
-            else if (BitsPerSecond < 1024 * 1024 * 1024)
-            {
-                return $"{BitsPerSecond / (1024.0 * 1024.0)}Mibps";
-            }
-            else if (BitsPerSecond < 1024L * 1024 * 1024 * 1024)
-            {
-                return $"{BitsPerSecond / (1024.0 * 1024.0 * 1024.0)}Gibps";
-            }
-            else
-            {
-                return $"{BitsPerSecond / (1024.0 * 1024.0 * 1024.0 * 1024.0)}Tibps";
-            }
+            _size = bps;
         }
+
+        public static readonly Speed Unlimited = new Speed(0);
+
+        public bool IsUnlimited => BitsPerSecond == 0;
+
+        public static Speed Parse(string input) => new Speed(DataSize.Parse(input));
+
+        public long BitsPerSecond => _size.Bits;
 
         public int CompareTo(Speed other)
         {
@@ -108,5 +48,10 @@ namespace MultiCache.Network
         {
             return left.CompareTo(right) >= 0;
         }
+
+        public override string ToString() => $"{base.ToString()}ps";
+
+
+        private readonly DataSize _size;
     }
 }
